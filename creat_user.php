2a031ba67c02e8b8a163e84c0b07fb11485c615e
kajkage <73479384+kajkage@users.noreply.github.com>
2021-06-07 10:53:22 +0200

    Update creat_user.php

--- creat_user.php
+++ creat_user.php
@@ -6,11 +6,13 @@ connect();
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-    <h2> Oprettelse af bruger til IKEA AUCTION </h2>
+    <h2> Oprettelse af bruger til IKEA AUKTIONER </h2>
     <meta charset="utf-8">
     <title>Ny bruger</title>
   </head>
   <body>
-<input type="text" name="" value="">
+    <label for="">E-mail</label>
+<input type="email" name="email" class="login_email" required>
+<input type="password" name="password" class="password" required>
   </body>
 </html>
