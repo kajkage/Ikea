ae77abb6e7c712874ff40c8f035e251f39122411
MathiasJN <Mathiasjn@outlook.dk>
2021-06-07 10:27:09 +0200

    Opdatering login

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
       <label for="">Email</label>
       <input type="text" class="login_email" required>
     </div>
-    <div class="login_password">
+    <div class="login">
       <label for="">Password</label>
-      <input type="password" class="form-control" required>
+      <input type="password" class="login_password" required>
     </div>
 
    <a href="..." class="btn">Login</a>
-   <a href="creat_user.php" class="btn">Creat User</a>
+   <a href="creat_user.php" class="btn">Opret Bruger</a>
 </div>
