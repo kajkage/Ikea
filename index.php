a0d832dd4e7773f3cf7a6aa83f1371e82549a6ca
MathiasJN <Mathiasjn@outlook.dk>
2021-06-07 10:14:00 +0200

    Login
    Login til auctions system

---
+++ index.php
@@ -0,0 +1,18 @@
+<div class="container">
+  <form action="">
+    <h1>IKEA AUCTIONS</h1>
+    <div class="form-group">
+      <label for="">User Name</label>
+      <input type="text" class="form-control" required>
+    </div>
+    <div class="form-group">
+      <label for="">Password</label>
+      <input type="password" class="form-control" required>
+    </div>
+  <form method="get" action="...">
+    <button href type="button">Login</button>
+  </form>
+  <form method="get" action="creat_user.php">
+    <button type="button">Create User</button>
+  </form>
+</div>
