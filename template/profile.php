d9da5b9b30d5ac3bbc5cd3c992387a2e9f07a1e0
kajkage <73479384+kajkage@users.noreply.github.com>
2021-06-08 16:23:37 +0200

    Create profile.php
    profil

---
+++ template/profile.php
@@ -0,0 +1,9 @@
+<?php
+$pid = $_GET["profile"];
+$sql = "SELECT * FROM users where user_id = '$pid'";
+
+$result = mysqli_query($con, $sql);
+
+$profile = mysqli_fetch_array($result);
+
+echo $profile['first_name'];
