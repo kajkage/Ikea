da875628975689c56d4ca0f419af6d0e781545e5
kvn27401 <73479557+kvn27401@users.noreply.github.com>
2021-06-09 11:00:08 +0200

    profile startet

--- template/auc.php
+++ template/auc.php
@@ -1,9 +1,11 @@
 <?php
 global $con;
 
-$pid = $_GET["cat"];
+$pid = $_GET["auc"];
 $sql = "SELECT * FROM actionusernobids where category_id = '$pid'";
 
 $result = mysqli_query($con, $sql);
 
  while($auc = mysqli_fetch_array($result)) {
+echo "Titel: " . $auc['title'] . "<br>Text: " . $auc['text'] . "<br>Auktion slutter d. " . $auc['time_end'];
+ }
