cd8fb9a4af3905cf7e71dcfb7e2fdbd09b5336f7
MathiasJN <Mathiasjn@outlook.dk>
2021-06-11 10:53:31 +0200

    Css

--- template/categories.php
+++ template/categories.php
@@ -1,12 +1,13 @@
+
+<div class="container">
 <?php
-
-
 $sql= "SELECT category_id, cat_name FROM categories";
 $result = mysqli_query($con, $sql);
 if (mysqli_num_rows($result) > 0) {
   while($row = mysqli_fetch_assoc($result)) {
-        ?><a href="?cat=<?php echo $row['category_id']; ?>"> <?php echo $row["cat_name"]. "<br>";
+        ?> <div class="container"><a id="cat" href="?cat=<?php echo $row['category_id']; ?>"> <?php echo $row["cat_name"]. "<br>";
     }
 }
 
 ?>
+</div>
