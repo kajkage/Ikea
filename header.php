8ea9f9cf8930ade971cf3c1bcac72a0ded287a8d
MathiasJN <Mathiasjn@outlook.dk>
2021-06-11 09:47:15 +0200

    Css på header

--- header.php
+++ header.php
@@ -1,10 +1,15 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
+    <link rel="stylesheet" href="css/header.css"
     <meta charset="utf-8">
     <title>IKEA AUKTION</title>
+
   </head>
   <body>
+    <div class="navbar">
+      <img scr="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Ikea_logo.svg/1200px-Ikea_logo.svg.png" width="1000" height="500" alt="logo"/>
+
     <?php foreach(getNav() as $navpage) {
       if($navpage['page_id'] == 4 && ! empty($_SESSION['logged_in'])) { ?>
         <li>
@@ -32,3 +37,5 @@
       </a> <?php
     }
   }
+  ?>
+</div>
